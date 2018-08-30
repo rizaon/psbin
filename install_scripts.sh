4168b3d6fb9d7a0b4f5726f5d64f527f9a4c025e
Riza Suminto <riza.on@gmail.com>
2018-08-30 17:35:06 -0600

    update install scripts

--- install_scripts.sh
+++ install_scripts.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 
@@ -7,6 +7,13 @@ export PSBIN=$dir
 source \$PSBIN/hadoop-rc.sh
 EOF
 
-cat /tmp/psbin-source.sh >> ~/.bashrc
-cat /tmp/psbin-source.sh >> ~/.zshrc_local
+# append this on top of .bashrc
+touch ~/.bashrc
+cat /tmp/psbin-source.sh ~/.bashrc >> /tmp/temp-bashrc
+mv /tmp/temp-bashrc ~/.bashrc
+
+#append this on top of .zshrc_local
+touch ~/.zshrc_local
+cat /tmp/psbin-source.sh ~/.zshrc_local >> /tmp/temp-zshrc_local
+mv /tmp/temp-zshrc_local ~/.zshrc_local
 
