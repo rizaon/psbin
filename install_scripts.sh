fe93a1b9c135c5e2aeca090f31922970b2abb0ac
Riza Suminto <riza.on@gmail.com>
2018-08-30 23:20:24 +0000

    add missing escape slash

--- install_scripts.sh
+++ install_scripts.sh
@@ -4,7 +4,7 @@ dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 
 cat << EOF > /tmp/psbin-source.sh
 export PSBIN=$dir
-source $PSBIN/hadoop-rc.sh
+source \$PSBIN/hadoop-rc.sh
 EOF
 
 cat /tmp/psbin-source.sh >> ~/.bashrc
